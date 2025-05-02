c31dfb4054b3b7298e7d2633194420ee18b73735
Jungyeon Kim <wjddus0904@naver.com>
2025-05-02 16:32:25 +0900

    Baseball game and TDD implementation

--- BaseballKATA/baseball_test.cpp
+++ BaseballKATA/baseball_test.cpp
@@ -1,11 +1,51 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
+using namespace testing;
 
-TEST(BaseballGame, TryGameTest) {
-	EXPECT_EQ(1, 1);
-	EXPECT_EQ(1, 1);
-	EXPECT_EQ(1, 1);
+class BaseballFixture : public testing::Test {
+public:
+	Baseball game{ "123" };
+	void assertIllegalArgument(string guessNumber) {
+		try {
+			game.guess(string(guessNumber));
+			FAIL();
+		}
+		catch (exception e) {
+			// PASS
+		}
+	}
+};
+
+TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
+	assertIllegalArgument("12");
+	assertIllegalArgument("12S");
+	assertIllegalArgument("121");
 }
+
+TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber) {
+	GuessResult result = game.guess("123");
+
+	EXPECT_TRUE(result.solved);
+	EXPECT_EQ(3, result.strikes);
+	EXPECT_EQ(0, result.balls);
+}
+
+TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber1) {
+	GuessResult result = game.guess("124"); // 2 Strikes, 0 Ball
+
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(2, result.strikes);
+	EXPECT_EQ(0, result.balls);
+}
+
+TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber2) {
+	GuessResult result = game.guess("321"); // 1 Strikes, 2 Ball
+
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(1, result.strikes);
+	EXPECT_EQ(2, result.balls);
+}
+
 
 int main() {
 	::testing::InitGoogleMock();
